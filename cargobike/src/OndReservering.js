7d530853421466a543e9b6f2222131c1af5e0e4a
FrankvanMeurs <79092346+FrankvanMeurs@users.noreply.github.com>
2021-04-20 22:05:15 +0200

    Recente bezorging
    Recente bezorging cards toegevoegd, overflow gefixt, header position veranderd.

--- cargobike/src/OndReservering.js
+++ cargobike/src/OndReservering.js
@@ -13,8 +13,9 @@ function OndReservering() {
             <Container className="DasboardGreeting">
                 <Row>
                     <Col xs={12} md={8}>
-                        <div className="Dash_greet"></div>
+                        <div className="Dash_greet">
                         <h1 className="Header_title">Mijn bezorgingen</h1>
+                        </div>
                     </Col>
                     <Col xs={12} md={8}>
                         <h1 className="Header_desc">Een overzicht van alle bezorgingen.</h1>
@@ -57,6 +58,139 @@ function OndReservering() {
             </Row>
             </div>
         </Container>
+
+        <Container className="Title_dash">
+                <Row>
+                    <Col xs={12} md={8}>
+                        <h1 className="Bezorging_title">Al uw gemaakte bezorgingen</h1>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container>
+             <div className="Card_bezorgingen box">
+            <Row>
+
+             <Col xs={10}>
+                     <h1 className="Card_title">Volgende bezorging</h1> 
+               </Col>
+               <Col xs={2}>
+                <IconContext.Provider value={{ color: "#838383", className: "Delete_icon", size: "2em" }}>
+                      <MdDelete />
+                     </IconContext.Provider>
+               </Col>
+
+               <Col xs={8} className="Cargobike_res_details">
+                     <h1>P+R Reitdiep</h1> 
+                     <h2>Friesestraatweg 152</h2>
+                     <h3>34 pakketten</h3>  
+               </Col>
+               <Col xs={4} className="Cargobike_res_details">
+                     <h1>Do: 01-04</h1> 
+                     <h2>12:00</h2>
+               </Col>
+
+               <Col xs={8}>
+                     <h3>Reservering wijzigen</h3> 
+               </Col>
+
+            </Row>
+            </div>
+        </Container>
+
+        <Container>
+             <div className="Card_bezorgingen box">
+            <Row>
+
+             <Col xs={10}>
+                     <h1 className="Card_title">Volgende bezorging</h1> 
+               </Col>
+               <Col xs={2}>
+                <IconContext.Provider value={{ color: "#838383", className: "Delete_icon", size: "2em" }}>
+                      <MdDelete />
+                     </IconContext.Provider>
+               </Col>
+
+               <Col xs={8} className="Cargobike_res_details">
+                     <h1>P+R Reitdiep</h1> 
+                     <h2>Friesestraatweg 152</h2>
+                     <h3>34 pakketten</h3>  
+               </Col>
+               <Col xs={4} className="Cargobike_res_details">
+                     <h1>Do: 01-04</h1> 
+                     <h2>12:00</h2>
+               </Col>
+
+               <Col xs={8}>
+                     <h3>Reservering wijzigen</h3> 
+               </Col>
+
+            </Row>
+            </div>
+        </Container>
+
+        <Container>
+             <div className="Card_bezorgingen box">
+            <Row>
+
+             <Col xs={10}>
+                     <h1 className="Card_title">Volgende bezorging</h1> 
+               </Col>
+               <Col xs={2}>
+                <IconContext.Provider value={{ color: "#838383", className: "Delete_icon", size: "2em" }}>
+                      <MdDelete />
+                     </IconContext.Provider>
+               </Col>
+
+               <Col xs={8} className="Cargobike_res_details">
+                     <h1>P+R Reitdiep</h1> 
+                     <h2>Friesestraatweg 152</h2>
+                     <h3>34 pakketten</h3>  
+               </Col>
+               <Col xs={4} className="Cargobike_res_details">
+                     <h1>Do: 01-04</h1> 
+                     <h2>12:00</h2>
+               </Col>
+
+               <Col xs={8}>
+                     <h3>Reservering wijzigen</h3> 
+               </Col>
+
+            </Row>
+            </div>
+        </Container>
+
+        <Container>
+             <div className="Card_bezorgingen box">
+            <Row>
+
+             <Col xs={10}>
+                     <h1 className="Card_title">Volgende bezorging</h1> 
+               </Col>
+               <Col xs={2}>
+                <IconContext.Provider value={{ color: "#838383", className: "Delete_icon", size: "2em" }}>
+                      <MdDelete />
+                     </IconContext.Provider>
+               </Col>
+
+               <Col xs={8} className="Cargobike_res_details">
+                     <h1>P+R Reitdiep</h1> 
+                     <h2>Friesestraatweg 152</h2>
+                     <h3>34 pakketten</h3>  
+               </Col>
+               <Col xs={4} className="Cargobike_res_details">
+                     <h1>Do: 01-04</h1> 
+                     <h2>12:00</h2>
+               </Col>
+
+               <Col xs={8}>
+                     <h3>Reservering wijzigen</h3> 
+               </Col>
+
+            </Row>
+            </div>
+        </Container>
+
         </div>
     )
 }
