7f37418d55184f2b17043ec66b4e395942c7de09
Makanmi <44712776+Makanmi@users.noreply.github.com>
2021-05-20 22:30:06 +0200

    Voor deze sprint, tot hier.
    De koppeling tussen het slotje en de statusstring is is nog inconsistent een paar seconde nadat de pagina is geladen. Ik weet (nog) niet waarom. Het slotje kleur heb ik voor nu (neutraal) grijs geworden. Bovenaan zitten 3 blokjes met hoeveel cargobikes vergrendeld, ontgrendeld en onbeschikbaar zijn, maar deze zijn nog niet gekoppeld aan de slotjes.

--- cargobike/src/components/CbStatuscard.js
+++ cargobike/src/components/CbStatuscard.js
@@ -2,13 +2,57 @@ import React, { useState } from 'react';
 import {Container, Row, Col } from 'react-bootstrap';
 import '../CbStatuscard.css';
 
-//test
-import CustomizedSwitches from '../components/fortheloveof';
+//Switch styling
+import { withStyles } from "@material-ui/core/styles";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import LockIcon from '@material-ui/icons/Lock';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+const CbSwitch = withStyles({
+    root: {
+      overflow: 'visible',
+    },
+    switchBase: {
+      color: "#FFFFFF",
+      backgroundColor: "#88C053",
+      "&$checked": {
+        color: "#FFFFFF",
+        backgroundColor: "#3F51B8"
+      },
+      "&$checked + $track": {
+        backgroundColor: "#FFFFFF"
+      },
+      '&$checked $thumb': {
+        backgroundColor: "#3F51B8"
+      },
+    },
+    thumb: {
+      width: 40,
+      height: 40,
+      borderRadius: 0,
+      boxShadow: 'none',
+    },
+    checked: {},
+    track: {backgroundColor: "#FFFFFF"}
+  })(Switch);
 
 export default function CbStatuscard(props){
     const{nr, afstand} = props;
-    //Lock: 'Op slot', 'Open', 'Niet beschikbaar'
-    const [cbStat, setcbStat] = useState('Open');
+    
+    //Lock
+    const [cbLockState, setcbLockState] = useState({checkedA: true,});
+    const handleChange = (event) => {
+        //Does not work consistently
+        if(cbLockState.checkedA == false){
+            setStatus('Vergrendeld')
+        }else{
+            setStatus('Ontgrendeld')
+        }
+        setcbLockState({ ...setcbLockState, [event.target.name]: event.target.checked });
+    };
+
+    const[status, setStatus] = useState('Ontgrendeld');
 
     return(
         <div className="cbStatCard">  
@@ -17,11 +61,23 @@ export default function CbStatuscard(props){
                     <Col xs={8} md={8}>
                         <h1>Nummer {nr}</h1>
                         <h2>{afstand} m</h2>
-                        <h3>Status: {cbStat}</h3>
+                        <h3>Status: {status}</h3>
                     </Col>
                     <Col xs={4} md={4}>
                         <div className="sw_container">
-                            <CustomizedSwitches/>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={
+                                    <CbSwitch
+                                        checked={setcbLockState.checkedA}
+                                        onChange={handleChange}
+                                        name="checkedA"
+                                        checkedIcon={<LockIcon color="white" fontsize="large"/>}
+                                        icon={<LockOpenIcon/>}
+                                    />
+                                    }
+                                />
+                            </FormGroup>
                         </div>
                     </Col>
                 </Row>
--- cargobike/src/pages/MonCbOverzicht.js
+++ cargobike/src/pages/MonCbOverzicht.js
@@ -2,12 +2,24 @@ import react from 'react';
 import {Container, Row, Col } from 'react-bootstrap';
 import '../MonCbOverzicht.css';
 import CbStatuscard from '../components/CbStatuscard'
+//Button styling
+import { Button } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
 
 export default function MonCbOverzicht(){
     return(
         <div className="MonCbOverzicht">
             <Container>
                 {/* Dashboard greeting */}
+                <Row>
+                    <Col xs={12} md={12} >
+                        <Button color="primary" ><ArrowBackIcon/> Terug</Button>
+                    </Col>
+                </Row>
                 <Row>
                     <Col xs={12} md={8} >
                         <h1 className="Dashgreeting">Cargobikes overzicht</h1>
@@ -18,15 +30,34 @@ export default function MonCbOverzicht(){
                 {/* List */}
                 <div className="contentbox">
                     <Row>
-                        <Col xs={12} md={8} >
-                            <h1 className="content">Cargobikes ontgrendelen</h1>
+                        <Col xs={6} md={6} >
+                            <h1 className="content">Cargobikes ontgrendelen</h1>        
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={3} md={3} className="cb_locked">
+                            <h1>Op slot</h1>
+                            <p>12</p>
+                                  
+                        </Col>
+                        <Col xs={3} md={3} className="cb_open">
+                            <h1>Open</h1>
+                            <p>8</p>
+                        </Col>
+                        <Col xs={3} md={3} className="cb_unavailable">
+                            <h1>Niet beschikbaar</h1>
+                            <p>1</p>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={12} >
                             <CbStatuscard nr="14" afstand="220" />
                             <CbStatuscard nr="42" afstand="899" />
                             <CbStatuscard nr="47" afstand="100" />
                             <CbStatuscard nr="23" afstand="230" />
                             <CbStatuscard nr="75" afstand="2500" />
                             <CbStatuscard nr="61" afstand="120" />
-
                         </Col>
                     </Row>
                 </div>
