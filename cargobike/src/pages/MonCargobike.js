aeadadff39229612ca51b0806cb6ee8026825783
winkenius <59976111+winkenius@users.noreply.github.com>
2021-05-28 01:31:21 +0200

    Component 'CargoLogCard.js' 1.0
    Added a card component for the MonCargobike page. This component is used to view log entries and add upon the pile. It's a first hardcoded draw, but it works and is imported to the MonCargobike page.

--- cargobike/src/pages/MonCargobike.js
+++ cargobike/src/pages/MonCargobike.js
@@ -1,11 +1,23 @@
 // any questions about this page can be asked to Isa Winkenius
 
 import React from 'react'
+import CargoLogCard from '../components/CargoLogCard';
+
+var cargobikeId = 1;
+var cargobikeSoort = 'Standaard Cargobike';
+var cargobikeStatus = 'C8';
 
 function CargobikePage() {
+
     return (
-        <p>hello</p>
+        <div>
+            <div>
+                <h1 className="Header_title">Nummer {cargobikeId}</h1>
+                <h3 className="">{cargobikeSoort} - {cargobikeStatus}</h3>
+            </div>
+             <CargoLogCard/>
+        </div>
     )
 }
 
-export default CargobikePage()
+export default CargobikePage
