a630997ee92ddbc0469ec82a80c6bc95116d24af
winkenius <59976111+winkenius@users.noreply.github.com>
2021-05-29 00:05:02 +0200

    small update to MonCargobike page

--- cargobike/src/pages/MonCargobike.js
+++ cargobike/src/pages/MonCargobike.js
@@ -4,10 +4,18 @@ import React from 'react'
 import CargoPicturesCard from '../components/CargoPicturesCard';
 import CargoLogCard from '../components/CargoLogCard';
 
+var cargobikeId = 12;
+var cargobikeSoort = 'Standaard Cargobike';
+var cargobikeStatus = 'C2';
+
 function CargobikePage() {
 
     return (
         <div>
+            <div>
+                <h1 className="Header_title">Nummer {cargobikeId}</h1>
+                <h3 className="">{cargobikeSoort} - {cargobikeStatus}</h3>
+            </div>
             <CargoPicturesCard/>
             <CargoLogCard/>
         </div>
