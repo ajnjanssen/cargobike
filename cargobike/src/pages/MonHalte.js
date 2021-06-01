aa2c5512b06a33cc4cd4998e4dc0483e6c3a792f
Makanmi <44712776+Makanmi@users.noreply.github.com>
2021-06-01 04:40:45 +0200

    Onderdeelstatus generator, sidescroller, and all buttons added

--- cargobike/src/pages/MonHalte.js
+++ cargobike/src/pages/MonHalte.js
@@ -1,21 +1,17 @@
 import React, { useState} from 'react';
-import GoogleMap from '../components/GoogleMap';
-import '../Googlemap.css';
-import {Container, Row, Col } from 'react-bootstrap';
-import styled from "styled-components";
-import CbStatCard from '../components/CbStatCard';
 
-const Contentbox = styled.div`
-    margin-top: 5vh;
-    background-color: white;
-    border-radius: 15px;
-    padding: 15px;
-    width: 100%;
-    margin-bottom: 10px;
-    display: inline-block;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
-    color: black;
-`
+//Styling
+import { Container } from 'react-bootstrap';
+import styled from "styled-components";
+
+//Components
+import GoogleMap from '../components/GoogleMap';
+import CbStatCard from '../components/CbStatCard';
+import OnderdeelStatus from '../components/OnderdeelStatus';
+import { Button } from '@material-ui/core';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import PhoneForwardedIcon from '@material-ui/icons/PhoneForwarded';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const Page_Header = styled.div`
     padding-top: 15vh;
@@ -32,6 +28,18 @@ const Navigation_p = styled.p`
     font-weight: 400;
 `
 
+const Contentbox = styled.div`
+    margin-top: 5vh;
+    background-color: white;
+    border-radius: 15px;
+    padding: 15px;
+    width: 100%;
+    margin-bottom: 10px;
+    display: inline-block;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+    color: black;
+`
+
 const ContentBox_h1 = styled.h1`
     color: #88C053;
     font-size: 20px;
@@ -42,12 +50,36 @@ const ContentBox_p = styled.p`
     color: #838383;
     font-weight: 600;
     font-size: 14;
+    margin: 0;
+    padding: 0;
 `
 
-  
+const ContentBottom = styled.div`
+    margin-top: 5vh;
+    background-color: white;
+    padding: 15px;
+    width: 100%;
+    margin-bottom: 10px;
+    display: inline-block;
+    color: black;
+`
+
+const ButtonContainer = styled.div`
+    margin: 3%;
+    float: right;
+`
+
+const Scrollbar = styled.div`
+display: flex;
+overflow-x: scroll;
+`
+
+const Scrollbar_item = styled.div`
+    margin: 20px;
+`
+
 export default function MonHalteMap(){
-    //Zernike default
-    const [halteNaam, setHalteNaam] = useState('P+R Reitdiep')
+    //Hoofdstation
     const [halteLat, setHalteLat ] = useState(53.2448297264615);
     const [halteLen, setHalteLen ] = useState(6.528572957354985);
 
@@ -55,7 +87,8 @@ export default function MonHalteMap(){
         <>
         <Container>
             <Page_Header>
-                <Navigation_h1>Halte 4 - oost west</Navigation_h1>
+                <Button><Navigation_p> <ArrowBackIcon/> Terug </Navigation_p> </Button>
+                <Navigation_h1>Halte 4 - Hoofdstation Groningen</Navigation_h1>
                 <Navigation_p>6 oplaadplekken en reparatie voorzieningen</Navigation_p>
             </Page_Header>
             <Contentbox>
@@ -68,7 +101,33 @@ export default function MonHalteMap(){
             </Contentbox>
         </Container>
         <GoogleMap locationMarkerlat={ halteLat } locationMarkerlng= { halteLen }/>
+        <ButtonContainer>
+            <a href="https://maps.google.com/?q=53.2109719,6.5619039">
+                <Button variant="contained" color="primary" size="large">
+                    Ga naar locatie <ArrowForwardIcon/>
+                </Button>
+            </a>
+        </ButtonContainer>
 
+        <ContentBottom>
+            <Container>
+                <ContentBox_h1>Status van onderdelen voor deze halte</ContentBox_h1>
+            </Container>
+            <Scrollbar>
+                <Scrollbar_item><OnderdeelStatus/></Scrollbar_item>
+                <Scrollbar_item><OnderdeelStatus/></Scrollbar_item>
+                <Scrollbar_item><OnderdeelStatus/></Scrollbar_item>
+                <Scrollbar_item><OnderdeelStatus/></Scrollbar_item>
+                <Scrollbar_item><OnderdeelStatus/></Scrollbar_item>
+            </Scrollbar>
+            <Container>
+                {/*<a href="tel: somephonenumber">*/}
+                    <Button>
+                        <ContentBox_h1> <PhoneForwardedIcon/> Bel Cargobike leverancier</ContentBox_h1>
+                    </Button>
+                {/* </a> */}
+            </Container>
+        </ContentBottom>
         </>
     )
 }
