713d44d8c2370a2cab125b97d9c0237cf3111dd0
StuntmanPackage <61024215+StuntmanPackage@users.noreply.github.com>
2021-06-16 19:48:27 +0200

    Data op bez dashboard kloppend gemaakt

--- cargobike/src/BezDashboard.js
+++ cargobike/src/BezDashboard.js
@@ -264,19 +264,19 @@ const [meldingen, setMeldingen] = useState([]);
       <Col xs={12} md={8}>
         <h1 className="RecentActivity">
           <ArrowForwardIosIcon className="Icon_Margin" style={{ fontSize: 14, color: "white" }} />
-          Bezorgroute voor 23-10-25, 11:15 Sontplein
+          Bezorgroute voor 23-10-'21, 11:15 Sontplein 5
         </h1>
   
         {/* {console.log(meldingen[0].afbeelding)} */}
-        <h2 className="RecentActivity_Desc">Route 54 pakketten</h2>
+        <h2 className="RecentActivity_Desc">Aantal: 54 pakketten</h2>
       </Col>
 
       <Col xs={12} md={8}>
         <h1 className="RecentActivity2">
           <ArrowForwardIosIcon className="Icon_Margin" style={{ fontSize: 14, color: "white" }} />
-          Bezorgroute voor 18-09-25, 13:55 Zernike
+          Bezorgroute voor 24-10-'21, 13:55 Zernikeplein 11
         </h1>
-        <h2 className="RecentActivity_Desc2">Route 31 pakketten</h2>
+        <h2 className="RecentActivity_Desc2">Aantal: 31 pakketten</h2>
       </Col>
 
       <ActRouteBez />
--- cargobike/src/components/employee/CurrentAgenda.js
+++ cargobike/src/components/employee/CurrentAgenda.js
@@ -6,14 +6,14 @@ function CurrentAgenda() {
         <div>
             <Col xs={8} className="Cargobike_res_details">
                 <div className="Agenda_punt">
-                <h4>Dinsdag 30-03-25 - 11:15</h4> 
-                <h1>Route P+R Reitdiep </h1>
-                <h2>34 pakketten</h2>  
+                <h4>Dinsdag 23-10-'21 - 11:15</h4> 
+                <h1>Sontplein 5</h1>
+                <h2>54 pakketten</h2>  
                 </div>
                 <div className="Agenda_punt">
-                <h4>Dinsdag 30-03-25 - 13:20</h4> 
-                <h1>Route P+R Meeerstad</h1>
-                <h2>26 pakketten</h2>  
+                <h4>Dinsdag 24-10-'21 - 13:55</h4> 
+                <h1>Zernikeplein 11</h1>
+                <h2>31 pakketten</h2>  
                 </div>
             </Col>
         </div>
