35168590c8bf850303c5d1dfd9d35afe7f74003d
nymph44 <46926580+nymph44@users.noreply.github.com>
2021-06-16 12:40:06 +0200

    Godver

--- cargobike/src/pages/MonDashboard.js
+++ cargobike/src/pages/MonDashboard.js
@@ -55,6 +55,14 @@ const ContentBox_h1 = styled.h1`
     margin-top: 40px;
 `
 
+const ContentBox_h1_alt = styled.h1`
+    color: #88C053;
+    font-size: 20px;
+    font-weight: 600;
+    margin-top: 40px;
+    margin-left:24px;
+`
+
 const useStyles = makeStyles({
     root: {
       background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -70,126 +78,6 @@ const useStyles = makeStyles({
         minWidth: 100
     }
   });
-
-// const Send__delivery = styled.div`
-//   width:400px;
-//   overflow-x: hidden;
-// `;
-
-// const HeaderContainer = styled.div`
-//     /* display:flex; */
-//     align-items:center;
-//     width:80%;
-// `
-
-// const ThisCol = styled.div`
-//   display: flex;
-//   justify-content: safe-center;
-//   padding-right: 100px;
-//   border-radius: 15px;
-//   :hover {
-//     background-color: #83b555;
-//     width: 80px;
-//     border-radius: 15px;
-//     cursor:pointer;
-//   }
-// `;
-
-// const ThisRow = styled.div`
-//   margin-left: 35px;
-//   > h1 {
-//     color: white !important;
-//   }
-// `;
-
-// const ThatRow = styled.div`
-//   margin-left: 36px;
-//   > h3 {
-//     color: white !important;
-//     font-weight:100;
-//   }
-// `;
-
-// const MakeSendOrder = styled.div`
-//     display:flex;
-//     flex-direction: column;
-    
-//     align-items:flex-start;
-    
-//     /* flex-wrap: wrap; */
-//     /* justify-content:center; */
-//     /* align-items:center; */
-//     border-radius:25px;
-//     margin-top:10px;
-//     margin-bottom: 30px;
-//     width:100%;
-//     height:420px;
-//     margin-left:25px;
-//     background:white;
-//     overflow: hidden;
-
-//     > Row {
-//         padding:25px;
-//     }
-
-//     > Row > h1 {
-//         font-size: 24px;
-//         font-weight: 600;
-//         color: #9cd06b;
-//     }
-
-//     > Row date {
-//         margin-left:140px;
-//         padding-left:100px;
-//     }
-
-//     > Row > .MuiInputBase-input {
-//         font: inherit;
-//         color: currentColor;
-//         width: 290px;
-//         border: 0;
-//         height: 1.1876em;
-//         margin: 0;
-//         display: block;
-//         padding: 6px 0 7px;
-//         min-width: 0;
-//         background: none;
-//         box-sizing: content-box;
-//         animation-name: mui-auto-fill-cancel;
-//         letter-spacing: inherit;
-//         animation-duration: 10ms;
-//         -webkit-tap-highlight-color: transparent;
-//     }
-// `;
-
-// const TimePicker = styled.div`
-//     margin-top: 20px;
-//     margin-left: 25px;
-//     width: 85%;
-//     display:flex;
-//     flex-direction:column;
-//     align-items:stretch;
-    
-// `
-
-// const TextCol = styled.div`
-//     width:100%;
-//     height:100%;
-//     display:table;
-//     margin-top:10px;
-// `
-
-// const TextCol2 = styled.div`
-//     width:100%;
-//     height:100%;
-//     display:table;
-//     margin-top:27px;
-// `
-
-// const ButtonContainer = styled.div`
-//     padding-left:25px;
-//     margin-top:20px;
-// `
 
 const Contentbox = styled.div`
     background-color: white;
@@ -238,9 +126,9 @@ const Info_image = styled.img`
 
 function OndDashboard() {
 
-            // this event dispatcher gives the event listner the news we want a certain navigation for this user.
-            const event = new Event('showNavbarMon');
-            window.dispatchEvent(event);
+    // this event dispatcher gives the event listner the news we want a certain navigation for this user.
+    const event = new Event('showNavbarMon');
+    window.dispatchEvent(event);
 
     const history = useHistory();
 
@@ -319,7 +207,7 @@ function OndDashboard() {
         
             {/* Cards met Cargobike modellen*/}
             <Container>
-                <ContentBox_h1>Bekijk status van Cargobikes</ContentBox_h1>
+                <ContentBox_h1_alt>Bekijk status van Cargobikes</ContentBox_h1_alt>
             </Container>
             
             <Scrollbar>
