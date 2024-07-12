092557474c218fe2c483ed5349c15f3ab73cb6ca
Alexander Janssen <46926580+ajnjanssen@users.noreply.github.com>
2024-07-12 13:12:14 +0200

    Update Firebase.js
    Remove development connection string

--- cargobike/src/components/firebase/Firebase.js
+++ cargobike/src/components/firebase/Firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 import 'firebase/storage'
 
 const FirebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDw1ZmeFabmO9gnBNtLpASw7QKsyXKe6FQ",
-  authDomain: "cargo-delivery-4f4e5.firebaseapp.com",
-  projectId: "cargo-delivery-4f4e5",
-  storageBucket: "cargo-delivery-4f4e5.appspot.com",
-  messagingSenderId: "256212066878",
-  appId: "1:256212066878:web:6c4cd55427e9661b18a76f",
-  measurementId: "G-VLXL4YX63T"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 const db = FirebaseApp.firestore();
